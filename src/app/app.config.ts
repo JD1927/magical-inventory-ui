bd3af23c439f95a8f0834e8f1cf6afc8a13558ff
Juan Aguirre <j1927aguirre@gmail.com>
2025-09-14 19:15:40 -0500

    feat: integrate PrimeNG with custom theme and update app structure
    - Added PrimeNG configuration in app.config.ts with a custom theme preset.
    - Simplified app.html by removing placeholder content and added a button component.
    - Updated app.ts to import ButtonModule from PrimeNG.
    - Set the HTML document to use a dark theme by default in index.html.
    - Created a new preset.prime.ts file to define a custom theme using PrimeUIX.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import CustomPreset from './preset.prime';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: CustomPreset,
+        options: {
+          darkModeSelector: '.magical-inventory-ui-dark',
+          zIndex: {}
+        },
+      },
+      zIndex: {
+        modal: 1100, // dialog, sidebar
+        overlay: 1000, // dropdown, overlaypanel
+        menu: 1000, // overlay menus
+        tooltip: 1100, // tooltip
+      },
+      inputVariant: 'outlined',
+      ripple: true,
+    }),
+  ],
 };
