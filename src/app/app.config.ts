2bf0a06832415bdeb3b6181f5ed7f235743b42f6
Juan Aguirre <j1927aguirre@gmail.com>
2025-10-11 16:34:52 -0500

    feat: add @ngrx/effects to dependencies and configure effects provider in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import type { ApplicationConfig } from '@angular/core';
 import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
 import { providePrimeNG } from 'primeng/config';
 import { APP_ROUTES } from './app.routes';
 import { Noir } from './preset.prime';
@@ -15,6 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     // Added for PrimeNG
     provideAnimations(),
+    provideEffects(),
     providePrimeNG({
       theme: {
         preset: Noir,
