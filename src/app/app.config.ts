65da4f2a21b7ea66dbdd00618f5931233f642717
Juan Aguirre <j1927aguirre@gmail.com>
2025-10-13 10:54:53 -0500

    feat: replace @ngrx/effects with @ngrx/operators in package.json and update pnpm lockfile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import type { ApplicationConfig } from '@angular/core';
 import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideEffects } from '@ngrx/effects';
 import { providePrimeNG } from 'primeng/config';
 import { APP_ROUTES } from './app.routes';
 import { Noir } from './preset.prime';
@@ -16,7 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     // Added for PrimeNG
     provideAnimations(),
-    provideEffects(),
     providePrimeNG({
       theme: {
         preset: Noir,
