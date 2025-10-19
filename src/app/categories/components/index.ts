e96b344478a8c36aa9cb2ed39eb025e0cf3f3fcd
Juan Aguirre <j1927aguirre@gmail.com>
2025-10-19 10:42:43 -0500

    feat(categories): add category list page to handle the table and the form

---
+++ src/app/categories/components/index.ts
@@ -0,0 +1,2 @@
+export * from './categories-table/categories-table';
+export * from './category-form/category-form';
--- src/app/categories/pages/category-list-page/category-list-page.ts
+++ src/app/categories/pages/category-list-page/category-list-page.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CategoriesTable, CategoryForm } from '@categories/components';
+import {
+  CategoriesStore,
+  DeleteCategoryStore,
+  deleteCategoryApiEvents,
+  getAllCategoriesApiEvents,
+} from '@categories/store';
+import { Dispatcher, Events } from '@ngrx/signals/events';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-category-list-page',
-  imports: [],
+  imports: [CommonModule, CategoriesTable, CategoryForm, Card],
   templateUrl: './category-list-page.html',
   styleUrl: './category-list-page.css',
 })
-export class CategoryListPage {}
+export class CategoryListPage {
+  categoriesStore = inject(CategoriesStore);
+  deleteCategoryStore = inject(DeleteCategoryStore);
+  dispatcher = inject(Dispatcher);
+  events = inject(Events);
+
+  constructor() {
+    this.dispatcher.dispatch(getAllCategoriesApiEvents.load());
+    this.listenToCategoryListChanges();
+  }
+
+  listenToCategoryListChanges(): void {
+    this.events
+      .on(deleteCategoryApiEvents.deletedSuccess)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        this.dispatcher.dispatch(getAllCategoriesApiEvents.load());
+      });
+  }
+
+  onDeleteCategory(categoryId: string): void {
+    this.dispatcher.dispatch(deleteCategoryApiEvents.delete(categoryId));
+  }
+}
